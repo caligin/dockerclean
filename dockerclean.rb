3b111180daec0aa825f620593177d7a0fdb6951c
Tsukihara Caligin <caligin35@gmail.com>
2014-12-03 13:54:10 +0100

    ref: simplifying (see http://jimhoskins.com/2013/07/27/remove-untagged-docker-images.html)

--- dockerclean.rb
+++ dockerclean.rb
@@ -3,16 +3,7 @@
 module Dockerclean
 
     def Dockerclean.stopped
-        docker_ps_a_regex = /(\w+)\s+([\w\-\.:]+).*?(Exited|Up)/
-        output = `docker ps -a`
-        output.lines.drop(1).each do |line|
-            match = line.match(docker_ps_a_regex)
-            if match.nil? then
-                puts "Cannot parse line: #{line}"
-            else
-                puts `docker rm #{match[1]}` if match[3] == "Exited"
-            end
-        end
+        `docker rm $(docker ps -a -q)`
     end
 
     def Dockerclean.untagged
